957087993a710665fb34c333c8bc82dc44ab444b
bagashvilit <bagashvilit@allegheny.edu>
2020-12-04 20:57:23 +0400

    Add function to search for repositories

--- prediction/data_collection.py
+++ prediction/data_collection.py
@@ -28,3 +28,34 @@ def api_wait(githb):
     elif rl.search.remaining <= 2:
         reset_time = calendar.timegm(rl.search.reset.timetuple())
         wait(reset_time - current_time + 10.0)
+
+def search_repos(token,languages):
+    githb = Github(token)
+    repo_list = []
+    for language in languages:
+        repos = githb.search_repositories(query="language:{} size:>50000 stars:>10000".format(language),sort = 'updated', order = 'desc')
+        for repo in repos:
+            repos_stats = {
+                "language": repo.language,
+                "project_name": repo.full_name,
+                "total_commits": repo.get_commits().totalCount,
+                "html_url": repo.html_url
+            }
+            repo_list.append(repos_stats)
+    return repo_list
+
+def final_status(statuses,completion):
+    fails = ["error","failure","cancelled","timed_out","action_required","startup_failure"]
+    final_status = []
+    if not statuses and not completion:
+        final_status.append("N/A")
+    for status in fails:
+        if status in statuses:
+            final_status.append("fail")
+            break
+    if "pending" in statuses:
+        final_status.append("pending")
+    else:
+        final_status.append("pass")
+
+    return final_status[0]
