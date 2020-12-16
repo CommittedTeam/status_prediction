9b14c019fed009a5b2b7285c7664d8f8c4d6d0e6
bagashvilit <bagashvilit@allegheny.edu>
2020-12-16 19:07:14 +0400

    train_test split the data

--- prediction/random_forest.py
+++ prediction/random_forest.py
@@ -8,7 +8,8 @@ from sklearn.utils.class_weight import compute_class_weight
 from sklearn.metrics import confusion_matrix
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score 
+from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score, cross_validate
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("../data/traindata.csv",index_col=0)
 # encode the labels
@@ -25,7 +26,10 @@ feature_list = list(features.columns)
 # Convert to numpy array
 features = np.array(features)
 
-
+# construct the training and testing splits
+(trainData, testData, trainLabel, testLabel) = train_test_split(
+    features, labels, test_size=0.3, random_state=0, stratify=labels
+)
 # the model we are using, set class_weight to balanced because the classes are imbalanced
 
 model = RandomForestClassifier(random_state=0,class_weight="balanced")
@@ -34,10 +38,10 @@ model = RandomForestClassifier(random_state=0,class_weight="balanced")
 metric_names = ['f1', 'roc_auc', 'average_precision', 'accuracy', 'precision', 'recall']
 scores_df = pd.DataFrame(index=metric_names, columns=["Stratified-CV"]) # to store the scores
 
-scv = StratifiedKFold(n_splits=8)
+scv = StratifiedKFold(n_splits=10)
 
 for metric in metric_names:
-    score = cross_val_score(model, features, labels, scoring=metric, cv=scv).mean()
+    score = cross_val_score(model, trainData, trainLabel, scoring=metric, cv=scv).mean()
     scores_df.loc[metric] = [score]
 print(scores_df)
 
